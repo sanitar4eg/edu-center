711f336985b9f7a058b24d77ade2b7592fc215ca
Denis Mejuev <sanitar.92@gmail.com>
2015-06-23 00:10:21 +0300

    'Broken pipe' on prod fixed

--- src/main/groovy/edu/netcracker/EduCenterApplication.java
+++ src/main/groovy/edu/netcracker/EduCenterApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan(basePackages = "edu.netcracker")
 @EnableJpaRepositories(basePackages = "edu.netcracker.repository")
+@EnableTransactionManagement
 public class EduCenterApplication {
 
     public static void main(String[] args) {
